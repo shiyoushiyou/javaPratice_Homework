a20e52bc4fa6f05af67c473b40d732ebdb7a10de
SHIYOU-ASUS\kmrry <shuushuutan@gmail.com>
2023-03-09 10:39:40 +0900

    更新老師講解

--- mysql_Homework/Day1/HomeworkDay1_2.sql
+++ mysql_Homework/Day1/HomeworkDay1_2.sql
@@ -6,6 +6,8 @@ SELECT *FROM 気象観測 WHERE 月 = 6;
 
 --②6月以外のデータ
 SELECT *FROM 気象観測 WHERE 月 NOT IN(6);
+SELECT *FROM 気象観測 WHERE 月 !=6;
+SELECT *FROM 気象観測 WHERE 月 <>6;
 
 --③降水量が100未満のデータ
 SELECT *FROM 気象観測 WHERE 降水量 < 100;
@@ -14,10 +16,10 @@ SELECT *FROM 気象観測 WHERE 降水量 < 100;
 SELECT *FROM 気象観測 WHERE 降水量 > 200;
 
 --⑤最高気温が30以上のデータ
-SELECT *FROM 気象観測 WHERE 最高気温 > 30;
+SELECT *FROM 気象観測 WHERE 最高気温 >= 30;
 
 --⑥最低気温が0以下のデータ
-SELECT *FROM 気象観測 WHERE 最低気温 < 0;
+SELECT *FROM 気象観測 WHERE 最低気温 <= 0;
 
 --⑦3月、5月、7月のデータ
 SELECT *FROM 気象観測 WHERE 月 IN(3,5,7);
@@ -29,13 +31,13 @@ SELECT *FROM 気象観測 WHERE 月  NOT IN(3,5,7);
 SELECT *FROM 気象観測 WHERE 降水量<100 AND 湿度 < 50;
 
 --⑩最低気温が5未満か、最高気温が35より高いデータ
-SELECT *FROM 気象観測 WHERE 最低気温 < 5 AND 最高気温 > 35;
+SELECT *FROM 気象観測 WHERE 最低気温 < 5 OR 最高気温 > 35;
 
 --⑪湿度が60～79の範囲にあるデータ
 SELECT *FROM 気象観測 WHERE 湿度 BETWEEN 60 AND 70 ;
 
 --⑫観測データがない列のある月のデータ
-SELECT *FROM 気象観測 WHERE 湿度 IS NULL;
+SELECT *FROM 気象観測 WHERE 月 IS NULL,降水量 IS NULL,最高気温 IS NULL,最低気温 IS NULL,湿度 IS NULL;
 
 --練習　3-1
 --次のデータを取得するSQL文を作成してください。
@@ -51,7 +53,11 @@ SELECT *FROM 都道府県 WHERE 都道府県名 LIKE '愛%';
 
 --④都道府県名と県庁所在地が一致するデータ
 SELECT *FROM 都道府県 WHERE 都道府県名 LIKE 県庁所在地;
+SELECT *FROM 都道府県 WHERE 都道府県名 = 県庁所在地;
+
 
 --⑤都道府県名と県庁所在地が一致しないデータ
 SELECT *FROM 都道府県 WHERE 都道府県名 NOT LIKE 県庁所在地;
+SELECT *FROM 都道府県 WHERE 都道府県名 != 県庁所在地;
+
 
